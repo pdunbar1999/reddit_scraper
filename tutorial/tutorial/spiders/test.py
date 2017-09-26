40dd1e9190b019ef58b0ebba6539a4529c7027ee
pdunbar1999 <peterdunbar1999@gmail.com>
2017-09-26 17:18:17 -0500

    Damn debugging

--- tutorial/tutorial/spiders/test.py
+++ tutorial/tutorial/spiders/test.py
@@ -7,18 +7,28 @@ class MySpider(scrapy.Spider):
         'https://www.reddit.com/'
     ]
     
-
+    global pageNumber
+    pageNumber = 0
+    
     def parse(self, response):
+        global pageNumber
+        pageNumber = pageNumber + 1
         for title in response.css('p.title'):               
             yield {
                 'title' : title.css('a::text\n').extract(),
-                'url' : title.css('a::attr(href)\n').extract(), 
+                'url' : title.css('a::attr(href)\n')[0].extract(),
+                'pageNumber' : pageNumber,
                 }
             
         next_page = response.css('span.next-button a::attr(href)').extract_first()
-        yield {
-            'pageNumber' : 1
-            }
         yield response.follow(next_page, callback=self.parse)
             
-           
+            
+"""    def link_check(x, y):
+    link = y.css('a::attr(href)\n')[1].extract() #link
+    if x == '/domain/i.redd.it/': #if the check equals a link to reddit
+        reddit_link = response.urljoin(link) #join urls
+        return reddit_link #return it
+    else:
+        return link #return just the normal link, leads to somewhere else          
+"""
